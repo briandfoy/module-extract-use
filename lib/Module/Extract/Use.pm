5417e0f7b6f8f9ee9b2fadb536ee7dc6eca3ae48
brian d foy <briandfoy@pobox.com>
2024-02-05 16:12:25 -0500

    Update copyright year

--- lib/Module/Extract/Use.pm
+++ lib/Module/Extract/Use.pm
@@ -348,7 +348,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2008-2023, brian d foy C<< <briandfoy@pobox.com> >>. All rights reserved.
+Copyright © 2008-2024, brian d foy C<< <briandfoy@pobox.com> >>. All rights reserved.
 
 This project is under the Artistic License 2.0.
 
